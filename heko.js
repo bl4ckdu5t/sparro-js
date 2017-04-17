c00fef37530902fd62da5fac11cb909333139582
Joseph Rex <joerexko@gmail.com>
2017-04-17 07:27:40 -0500

    Initial commit for JS file

---
+++ heko.js
@@ -0,0 +1,13 @@
+function hk(){
+  var el = document.createElement('iframe');
+  var mq = window.matchMedia('(min-width: 500px)');
+  el.src='//heko-chat.herokuapp.com';
+  el.style.position = 'fixed';
+  el.style.bottom = 0;
+  el.style.right = 0;
+  el.style.border = 0;
+  el.style.width = mq.matches ? '300px' : 'calc(100% + 8px)';
+  el.style.height = '100%';
+  var inside = document.querySelector('.inside');
+  inside.appendChild(el);
+}
